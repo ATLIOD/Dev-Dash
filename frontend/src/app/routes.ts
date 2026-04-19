15c3b985c6f19d5b543c9c7c7d412c70866786c6
Amber Feeley <106501894+amberfeeley@users.noreply.github.com>
2026-04-19 17:05:32 -0500

    15: Implemented Data Mode Routes (#27)

--- frontend/src/app/routes.ts
+++ frontend/src/app/routes.ts
@@ -1,1 +1,13 @@
-// TO-DO #15: Set up frontend routing with React Router (Framework) -- https://reactrouter.com/start/framework/routing
+import { createBrowserRouter } from "react-router";
+import Home from "../pages/Home";
+import MainLayout from "../layout/MainLayout";
+
+// Additional routing information:
+// https://reactrouter.com/start/data/routing
+
+export const router = createBrowserRouter([
+  {
+    Component: MainLayout,
+    children: [{ index: true, Component: Home }],
+  },
+]);
