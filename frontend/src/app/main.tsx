1f5136ab0f15f97c556e29678f0736033a2c5089
Amber Feeley <106501894+amberfeeley@users.noreply.github.com>
2026-04-19 17:05:32 -0500

    15: Implemented Data Mode Routes (#27)

---
+++ frontend/src/app/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import { router } from "./routes";
+import { RouterProvider } from "react-router";
+import { StrictMode } from "react";
+import "../index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
