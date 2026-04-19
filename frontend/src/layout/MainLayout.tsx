f1a4bdb6d0055209bcabee9294c8c35eb2eabbb6
Amber Feeley <106501894+amberfeeley@users.noreply.github.com>
2026-04-19 17:05:32 -0500

    15: Implemented Data Mode Routes (#27)

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,3 +1,11 @@
+import { Outlet } from "react-router";
+
 export default function MainLayout() {
-  return <>{/* TO-DO #16: Set up main layout with Navbar, footer, and page outlet. */}</>;
+  return (
+    <>
+      {/* TO-DO #16: Set up main layout with Navbar, footer, and page outlet.
+    https://reactrouter.com/start/declarative/routing */}
+      <Outlet />
+    </>
+  );
 }
