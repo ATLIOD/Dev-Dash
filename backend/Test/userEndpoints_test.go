ce31dccb773389de36b47f4dd38b1b378f7ed27d
ATLIOD <60109469+ATLIOD@users.noreply.github.com>
2026-04-21 21:36:16 -0500

    23: pain and suffering

--- backend/Test/userEndpoints_test.go
+++
@@ -1,8 +0,0 @@
-package Integration
-
-//test user endpoints
-//first create aka user/userID with POST
-//store id from create response into initial memory variable for later use in get, update and delete tests
-//get aka user/userID with GET
-//update aka user/userID with PUT
-//delete aka user/userID with DELETE
