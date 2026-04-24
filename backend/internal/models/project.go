829a7c53652d50b39a5a5b2c10c0416f6a8d00e7
ATLIOD <60109469+ATLIOD@users.noreply.github.com>
2026-04-24 01:07:56 -0500

    24: lot of stuff to change with uuid now

--- backend/internal/models/project.go
+++ backend/internal/models/project.go
@@ -3,15 +3,15 @@ package models
 import "time"
 
 type Project struct {
-	// id or uuid we decide
-	ID            string    `json:"id"`
+	ID            int64     `json:"id"`
+	UUID          string    `json:"uuid"`
 	Name          string    `json:"name"`
 	Description   string    `json:"description"`
 	Status        string    `json:"status"` // e.g., "active", "completed", "archived"
 	Stack         string    `json:"stack"`
 	RepositoryURL string    `json:"repository_url"`
 	DeploymentURL string    `json:"deployment_url"`
-	UserID        string    `json:"user_id"`
+	UserID        int64     `json:"user_id"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
 }
@@ -23,7 +23,7 @@ type CreateProjectRequest struct {
 	Stack         string `json:"stack"`
 	RepositoryURL string `json:"repository_url"`
 	DeploymentURL string `json:"deployment_url"`
-	UserID        string `json:"user_id"`
+	UserID        int64  `json:"user_id"`
 }
 
 type UpdateProjectRequest struct {
@@ -36,21 +36,21 @@ type UpdateProjectRequest struct {
 }
 
 type ProjectResponse struct {
-	ID            string    `json:"id"`
+	ID            string    `json:"uuid"`
 	Name          string    `json:"name"`
 	Description   string    `json:"description"`
 	Status        string    `json:"status"`
 	Stack         string    `json:"stack"`
 	RepositoryURL string    `json:"repository_url"`
 	DeploymentURL string    `json:"deployment_url"`
-	UserID        string    `json:"user_id"`
+	UserID        int64     `json:"user_id"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
 }
 
 func (p *Project) ToResponse() ProjectResponse {
 	return ProjectResponse{
-		ID:            p.ID,
+		ID:            p.UUID,
 		Name:          p.Name,
 		Description:   p.Description,
 		Status:        p.Status,
