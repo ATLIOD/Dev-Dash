8c813d63f4dd798e6ecc97952033c94055c84898
Dominic Burfict <60109469+ATLIOD@users.noreply.github.com>
2026-05-22 21:30:14 -0500

    Update backend/Test/integration/user_test.go
    Co-authored-by: Amber Feeley <106501894+amberfeeley@users.noreply.github.com>

--- backend/Test/integration/user_test.go
+++ backend/Test/integration/user_test.go
@@ -18,7 +18,7 @@ import (
 )
 
 type UserResponse struct {
-	ID        string    `json:"uuid"`
+	UUID        string    `json:"uuid"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
